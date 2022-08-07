7c80de5b20a736d4008ed8c3f40a21883368704b
Noony-yay <naamabh10@gmail.com>
2022-08-07 15:59:41 +0300

    First JavaScript file!!

---
+++ exercise01.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    var box = document.getElementById('box');
+    if(box.style.backgroundColor=='lightblue') {
+        box.style.backgroundColor='purple';
+    }
+    else {
+        box.style.backgroundColor='lightblue';
+    }
+}
