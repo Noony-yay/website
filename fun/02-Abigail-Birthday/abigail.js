2c2025617ce7c01740655b5cdf443acc6213b2af
Noony-yay <naamabh10@gmail.com>
2022-08-20 16:42:36 +0300

    Hot air balloon in 12 hours!
    Yay!

---
+++ fun/02-Abigail-Birthday/abigail.js
@@ -0,0 +1,6 @@
+function buttonClick() {
+    var balloon = document.getElementById('image-wrapper');
+    var button = document.getElementById('button');
+    button.style.opacity = '0%';
+    balloon.style.top = '0%';
+}
