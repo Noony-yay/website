9fc96be313ab403d0a0416a14453547a2567f732
Noony-yay <naamabh10@gmail.com>
2023-05-25 12:00:25 +0300

    Fixed last bug + added distance
    There is a new bug with the distance thing, need to ask Dad
    
    Co-Authored-By: Zvika Ben-Haim <zvikabh@users.noreply.github.com>

--- fun/09/gps.js
+++ fun/09/gps.js
@@ -1,19 +1,38 @@
+var getToThisPoint = [[32.798370, 34.973407]]
+var pointNum = 0
+
 function gid(id) {
     return document.getElementById(id);
 }
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, cantLoad);
     }
     else {
-        gid('parameter-container').style.display = 'none';
-        gid('cant').style.display = 'block';
+        cantLoad();
     }
+}
+
+function cantLoad() {
+    gid('parameter-container').style.display = 'none';
+    gid('cant').style.display = 'block';
 }
 
 function showPosition(position) {
     gid('latitude').innerHTML = position.coords.latitude;
     gid('longitude').innerHTML = position.coords.longitude;
     gid('accuracy').innerHTML = position.coords.accuracy;
+    gid('distance').innerHTML = getDistance();
+}
+
+function getDistance(position) {
+    var lat1 = position.coords.latitude;
+    var lng1 = position.coords.longitude;
+    var lat2 = getToThisPoint[pointNum][0];
+    var lng2 = getToThisPoint[pointNum][1];
+    var latDistance = lat1 - lat2;
+    var lngDistance = lng1 - lng2;
+    var distance = Math.sqrt(latDistance*latDistance + lngDistance*lngDistance);
+    return distance;
 }
