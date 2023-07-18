9591677fb62f910d15b1db0f131cfa9811c06f81
Noony-yay <naamabh10@gmail.com>
2023-07-18 10:52:52 +0300

    Depth 2
    Co-Authored-By: Zvika Ben-Haim <zvikabh@users.noreply.github.com>

--- fun/11/tictactoe.js
+++ fun/11/tictactoe.js
@@ -96,22 +96,50 @@ function winnerDetected(winner) {
 
 /**
  * If the computer can win with one step, it takes this step.
- * If not, it chooses randomly.
+ * If not, it checks all options.
+ * For each option, if the player can win, it won't take the step that leads to this option.
+ * Then, it chooses randomly from the options that don't lead to the player winning.
  * @param cellsNow - list of the current board state.
  * @returns The number of the cell to play (1-9).
  */
 function computersTurn(cellsNow) {
-    var cellsAfter = [...cellsNow];
+    var cellsAfterD1 = [...cellsNow];
+    var okayMoves = [];
     for (var i = 0; i < 9; i++) {
-        if (cellsAfter[i] == "e") {
-            cellsAfter[i] = "o";
-            if (getWinner(cellsAfter) == "o") {
-                return i + 1;
-            }
-            cellsAfter = [...cellsNow];
+        if (cellsAfterD1[i] != "e") {
+            continue;  // Can't put an O here.
         }
+        cellsAfterD1[i] = "o";
+        if (getWinner(cellsAfterD1) == "o") {
+            console.log('Definite win!')
+            return i + 1;
+        }
+        // check if the player can win.
+        // if the player can win, then don't do this.
+        // if not, we might want to do this.
+        var canPlayerWin = false;
+        for (var j = 0; j < 9; j++) {
+            if (cellsAfterD1[j] != "e") {
+                continue;  // Can't put an X here.
+            }
+            var cellsAfterD2 = [...cellsAfterD1];
+            cellsAfterD2[j] = "x";
+            if (getWinner(cellsAfterD2) == "x") {
+                canPlayerWin = true;
+            }
+        }
+        if (!canPlayerWin) {
+            okayMoves.push(i + 1);
+        }
+        cellsAfterD1 = [...cellsNow];
     }
-    return randomEmptyCell(cellsNow);
+    if (okayMoves.length == 0) {
+        // If I will definitely lose, I won't tell the player so maybe they will be stupid!
+        console.log('Oh no, I lose.')
+        return randomEmptyCell(cellsNow);
+    }
+    console.log('I dont really know who will win. Okay moves: ' + okayMoves)
+    return okayMoves[Math.floor(Math.random() * okayMoves.length)];
 }
 /**
  * Returns a random number in the range 1-9.
