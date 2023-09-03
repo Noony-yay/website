6d1ed09b265d5bb1c416eba4cf471ec5900c70af
Noony-yay <naamabh10@gmail.com>
2023-09-03 15:41:11 +0300

    Word class

--- fun/08/english.js
+++ fun/08/english.js
@@ -1,18 +1,30 @@
-var words = [["add", "להוסיף"],
-            ["afraid", "מפחד"],
-            ["alligator", "תנין"],
-            ["ant", "נמלה"],
-            ["apple", "תפוח"],
-            ["arm", "זרוע"],
-            ["ask", "לשאול"],
-            ["baby", "תינוק"],
-            ["bad", "רע"],
-            ["bag", "תיק"],
-            ["ball", "כדור"],
-            ["balloon", "בלון"],
-            ["banana", "בננה"],
-            ["bat", "עטלף"],
-            ["bed", "מיטה"];
+class Word {
+    constructor(english, hebrew) {
+        this.english = english;
+        this.hebrew = hebrew;
+        this.numCorrect = 0;
+        this.numIncorrect = 0;
+    }
+}
+
+var words = [
+    new Word("add", "להוסיף"),
+    new Word("afraid", "מפחד"),
+    new Word("alligator", "תנין"),
+    new Word("ant", "נמלה"),
+    new Word("apple", "תפוח"),
+    new Word("arm", "זרוע"),
+    new Word("ask", "לשאול"),
+    new Word("baby", "תינוק"),
+    new Word("bad", "רע"),
+    new Word("bag", "תיק"),
+    new Word("ball", "כדור"),
+    new Word("balloon", "בלון"),
+    new Word("banana", "בננה"),
+    new Word("bat", "עטלף"),
+    new Word("bed", "מיטה")
+];
+
 var selectedWord;
 var numCorrectAnswers = 0;
 var numQuestionsAsked = 0;
@@ -36,24 +48,25 @@ function randomWord() {
     var englishWord = gid('english-word');
     var wordIndex = getRandomInt(words.length);
     selectedWord = words[wordIndex];
-    englishWord.innerHTML = selectedWord[0];
-    words.splice(wordIndex, 1);
+    englishWord.innerHTML = selectedWord.english;
 }
 function answered() {
     gid('finished-button').style.display = 'none';
     var answer = gid('translation').value;
     var cell = gid('cell' + numQuestionsAsked);
-    if (answer == selectedWord[1]) {
+    if (answer == selectedWord.hebrew) {
         var rightAnswer = gid('right-answer-container');
         rightAnswer.style.display = 'block';
         cell.style.backgroundColor = '#00c300';
         numCorrectAnswers += 1;
+        selectedWord.numCorrect += 1;
     }
     else {
-        gid('right-answer').innerHTML = selectedWord[1];
+        gid('right-answer').innerHTML = selectedWord.hebrew;
         var wrongAnswer = gid('wrong-answer-container');
         wrongAnswer.style.display = 'block';
         cell.style.backgroundColor = '#e70000';
+        selectedWord.numIncorrect += 1;
     }
     gid('continue-button').style.display = 'block';
     numQuestionsAsked +=1;
