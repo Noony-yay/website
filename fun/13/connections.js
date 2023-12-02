c4963ce65650dafc69bbf33bcafba7ceee92c5fb
Zvika Ben-Haim <zvikabh@gmail.com>
2023-12-02 11:45:50 +0200

    Allow selecting up to 4 items

--- fun/13/connections.js
+++ fun/13/connections.js
@@ -1,18 +1,70 @@
 'use strict';
 
-const initialSetting = [
-  []
-];
+const settings = {
+  initialBoard: [
+    [ 'לוי', 'כהן', 'דליה', 'שושנה' ],
+    [ 'חפציבה', 'נורית', 'גד', 'חניתה' ],
+    [ 'כומר', 'נעמי', 'דן', 'אשר' ],
+    [ 'שאמאן', 'דפנה', 'יפה', 'נזיר' ],
+  ],
+  correctAssignments: {
+    'שושנה': 0,
+    'נורית': 0,
+    'יפה': 0,
+    'נעמי': 0,
+    'אשר': 1,
+    'גד': 1,
+    'דן': 1,
+    'לוי': 1,
+    'כומר': 2,
+    'שאמאן': 2,
+    'נזיר': 2,
+    'כהן': 2,
+    'דליה': 3,
+    'חפציבה': 3,
+    'דפנה': 3,
+    'חניתה': 3,
+  },
+  groupDescriptors: [
+    'זמרות זוכות פרס ישראל',
+    'שבטי ישראל',
+    'אנשי דת',
+    'קיבוצים בצפון',
+  ]
+};
 
 function initializeHtml() {
-  for (let row = 1; row <= 4; ++row) {
-    for (let col = 1; col <= 4; ++col) {
+  for (let row = 0; row < 4; ++row) {
+    for (let col = 0; col < 4; ++col) {
       $('.grid-container').append(
         $('<div class="grid-item">').append(
           $('<div class="item-text">')
-        )
+        ).on('click', clickItem)
       );
     }
+  }
+  // TODO: Load settings via ajax
+  populateInitialBoard();
+}
+
+function populateInitialBoard() {
+  for (let row = 0; row < 4; ++row) {
+    for (let col = 0; col < 4; ++col) {
+      $('.item-text').eq(row * 4 + col).text(
+        settings.initialBoard[row][col]
+      );
+    }
+  }
+}
+
+function clickItem(evt) {
+  const gridItem = $(evt.currentTarget);
+  if (gridItem.hasClass('selected-item')) {
+    gridItem.removeClass('selected-item');
+    return;
+  }
+  if ($('.selected-item').length < 4) {
+    gridItem.addClass('selected-item');
   }
 }
 
