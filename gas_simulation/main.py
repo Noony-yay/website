6820da368d70baa73bd04e756b2707cdbc1d2aab
Noony-yay <naamabh10@gmail.com>
2025-07-17 14:33:30 +0300

    pressure~

--- gas_simulation/main.py
+++ gas_simulation/main.py
@@ -10,6 +10,7 @@ class Molecule:
         self.y = random.random() * length
         self.z = random.random() * length
         self.num_collisions: int = 0
+        self.total_delta_v: float = 0
     
     def update(self, length: float, step_duration: float):
         self.x += self.vx * step_duration
@@ -18,12 +19,15 @@ class Molecule:
         if self.x <= 0 or self.x >= length:
             self.vx *= -1
             self.num_collisions += 1
+            self.total_delta_v += abs(2*self.vx)
         if self.y <= 0 or self.y >= length:
             self.vy *= -1
             self.num_collisions += 1
+            self.total_delta_v += abs(2*self.vy)
         if self.z <= 0 or self.z >= length:
             self.vz *= -1
             self.num_collisions += 1
+            self.total_delta_v += abs(2*self.vz)
 
     def calc_kinetic_energy(self) -> float:
         vsquared = self.vx**2 + self.vy**2 + self.vz**2
@@ -41,7 +45,24 @@ class Simulator:
         return sum([mol.calc_kinetic_energy() for mol in self.mols]) # joules
     
     def calc_temp(self) -> float:
-        return self.calc_heat() / len(self.mols) / BOLTZMANN # kelvin
+        return self.calc_heat() / num_mols / BOLTZMANN # kelvin
+    
+    def calc_avg_acceleration(self) -> float:
+        """The average acceleration that a single molecule feels."""
+        total_delta_v = sum(mol.total_delta_v for mol in self.mols) # meters per second
+        return total_delta_v / num_mols / simulation_duration # meters per second^2
+    
+    def calc_avg_force(self) -> float:
+        """The average force that a single molecule feels."""
+        return mol_mass * self.calc_avg_acceleration() # Newton
+    
+    def calc_total_force(self) -> float:
+        """The total force applied to the container."""
+        return self.calc_avg_force() * num_mols # Newton
+    
+    def calc_pressure(self) -> float:
+        surface_area = length**2 * 6
+        return self.calc_total_force() / surface_area # Pascal
 
 
 length = 1 # meters
@@ -62,9 +83,11 @@ def main():
         for mol in main_simulator.mols:
             mol.update(length, step_duration)
         current_time += step_duration
-    print(f"{main_simulator.calc_collisions_per_sec():.2f} collisions per second.")
-    print(f"Heat: {main_simulator.calc_heat():.3g} joules.")
     temp = main_simulator.calc_temp()
-    print(f"Temperature: {temp:.2f} kelvin ({(temp - 273.15):.2f} celsius).")
+    surface_area = length**2 * 6 # meter^2
+    print(f"Molecule count  (n):   {num_mols}")
+    print(f"Volume          (V):   {length**3:.3f} meters^3")
+    print(f"Temperature     (T):   {temp:.2f} kelvin ({(temp - 273.15):.2f} celsius)")
+    print(f"Pressure        (P):   {main_simulator.calc_pressure():.3g} Pascal")
 
 main()
